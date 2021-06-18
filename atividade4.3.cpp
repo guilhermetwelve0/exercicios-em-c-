44ba1be40533ba654bb1c131a488fe9866229b16
guilhermetwelve0 <78167116+guilhermetwelve0@users.noreply.github.com>
2021-06-18 19:09:54 -0300

    Add files via upload

---
+++ atividade4.3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+int i;
+ do {
+ 	for ( i = 5; i <= 500 ; i+=5 ){
+	 printf ("Numero: %d\n",i);
+	 }
+}while( i < 501);{
+ 
+}
+return 0;
+}
