6b25b391e7a990e2f628a255443c4c9009081d69
guilhermetwelve0 <78167116+guilhermetwelve0@users.noreply.github.com>
2021-06-18 19:09:18 -0300

    Add files via upload

---
+++ atividade3.1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	int i;
+	for ( i = 100; i <= 200 ; i++ ){
+	printf ("Numero: %d\n",i);
+	}
+	return 0;
+}
---
+++ atividade3.2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+int i;
+ while( i <= 201){
+ 	for ( i = 100; i <= 200 ; i++ ){
+	 printf ("Numero: %d\n",i);
+	 }
+	return 0;
+}
+}
